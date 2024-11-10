422754fff4de7032998dfcb382ff284083b09767
IstiyaqAhmad <aistiyaq07@gmail.com>
2024-11-10 22:43:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+function navigateToHome() {
+    window.location.href = "index.html";
+}
+
+function navigateToHome() {
+    window.location.href = "index.html";
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+
